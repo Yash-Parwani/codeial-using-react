4bef3e6a51538a95721da936f6951e3de833f706
Yash Parwani <parwaniyash5610@gmail.com>
2022-05-07 20:38:25 +0530

    Custom Fetch 1

--- api/index.js
+++ api/index.js
@@ -1,6 +1,62 @@
 // custom fetch for api
 
-const customFetch = () =>{
+import { LOCALSTORAGE_TOKEN_KEY } from "../src/utils";
+
+const customFetch = async(url,{body,...customConfig}) =>{
+    /*
+    SOme api urls will require authorization i.e we need to send authorization key inside header bearer
+
+    say we want to create a post ,but a post will be created only for logged in users
+
+    So when we make a login request using api , we need to send this token
+    so everything will be inside this custom fetch function
+
+
+    */
+   // once user makes login request, he gets a token after successfull login, so we will store that token in local storage
+    const token = window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
+
+
+    const headers = {
+        'content-type' : 'application/json',
+        Accept: 'application/json'
+    }
+
+
+    if(token){
+        headers.Authorization = `Bearer ${token}`;
+    }
+    const config = {
+        ...customConfig,
+        headers : {
+            ...headers,
+            ...customConfig.headers,
+        },
+    };
+
+    if(body){
+        config.body = JSON.stringify(body);
+    }
+
+
+
+    try{
+     const response = await fetch(url,config);
+     const data = await response.json();
+     if(response.success){
+         return{
+             data: data.data,
+             success: true
+         };
+     }
+     throw new Error(data.message);
+    }catch(error){
+        console.error("Error");
+        return{
+           message : error.message,
+            success: false
+        };
+    }
 
 };
 
