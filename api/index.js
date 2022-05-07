606e432669d1ded859573c814365fd2d43ba166d
Yash Parwani <parwaniyash5610@gmail.com>
2022-05-07 20:24:37 +0530

    setting up api

---
+++ api/index.js
@@ -0,0 +1,14 @@
+// custom fetch for api
+
+const customFetch = () =>{
+
+};
+
+
+const getPosts=(page,limit)=>{
+
+    // page : page no , limit tells how many posts we want api to fetch for us at once
+
+    return customFetch()
+
+}
