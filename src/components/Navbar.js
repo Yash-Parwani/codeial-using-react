b6c90836ea0a59f211a2d4226fa82f6f917cf3f3
Yash Parwani <parwaniyash5610@gmail.com>
2022-05-18 21:52:45 +0530

    useAuth in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,58 @@
-import styles from '../styles/navbar.module.css'
+import { useAuth } from '../hooks';
+import styles from '../styles/navbar.module.css';
 
-const Navbar = () =>{
-    return (
-       <div>
-           <div className={styles.leftDiv}>
-                   <Link to="/" >
-                   <img alt=" " src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png" />
-                   </Link>
+const Navbar = () => {
+  const auth = useAuth();
+  return (
+    <div>
+      <div className={styles.leftDiv}>
+        <Link to="/">
+          <img
+            alt=" "
+            src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+          />
+        </Link>
+      </div>
 
-                   
-           </div>
+      <div className={styles.rightNav}>
+        {auth.user && (
+          <div className={styles.user}>
+            <Link to="/">
+              <img
+                src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                alt=""
+                className={styles.userDp}
+              />
+            </Link>
+            <span>{auth.user.name} </span>
+          </div>
+        )}
 
-           <div className={styles.rightNav}>
-               <div className={styles.user}>
-                     <Link to= "/" >
-                       <img 
-                        src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-              alt=""
-              className={styles.userDp} />
-                     </Link>
-                     <span>Yash</span>
-               </div>
-               <div className={styles.navLinks}></div>
-               <ul>
-                   <li>
-                       <Link to="/login" >
-                           Login
-                       </Link>
-                   </li>
-                   <li>
-                       <a href="/">
-                           Logout
-                        </a>
-                   </li>
-                   <li>
-                       <a href="/">
-                           Register
-                       </a>
-                   </li>
+        <div className={styles.navLinks}></div>
+        <ul>
+          {/* if user exists than show only logout else show login */}
+          {auth.user ? (
+            <>
+              <li>
+                <button onClick={auth.logout}>Logout</button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+            <Link to="/login">Login</Link>
+          </li>
 
-               </ul>
-
-
-           </div>
-       </div>
-    );
-}
-
+          <li>
+            <a href="/">Register</a>
+          </li>
+            </>
+          )}
+         
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
